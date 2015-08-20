364b4d59ea462ddc0d95d45a545c9fed83975eaf
federicojasson <federicojasson@gmail.com>
2015-08-20 13:19:56 -0300

    Initial config

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name marfedhausApp
+ * @description
+ * # marfedhausApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('marfedhausApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marfedhausApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the marfedhausApp
+ */
+angular.module('marfedhausApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
